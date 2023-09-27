a40a0c2ab2305d968a9ac8719264b942253c853f
Szentgyörgyi Levente <levente.sztgy@gmail.com>
2023-09-27 09:25:39 +0200

    commi2

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -79,6 +79,10 @@ public class GT4500 implements SpaceShip {
       case ALL:
         // try to fire both of the torpedo stores
         //TODO implement feature
+        if(!primaryTorpedoStore.isEmpty() && !secondaryTorpedoStore.isEmpty()){
+            firingSuccess=true;
+        }
+        else firingSuccess=false;
 
         break;
     }
